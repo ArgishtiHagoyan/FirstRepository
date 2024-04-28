64570b14976060514927eb4f2db8bbc6fcdf7973
ArgishtiHagoyan <167971661+ArgishtiHagoyan@users.noreply.github.com>
2024-04-28 10:00:29 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+try
+{
+    string path = "/Users/artyom/Desktop/csharp/firstfile.txt";
+    string path2 = "/Users/artyom/Desktop/csharp/secondfile.txt";
+    string sms = "bla-bla";
+
+    using (StreamWriter sw = new StreamWriter(path))
+    {
+        sw.WriteLine(sms);
+        sw.WriteLine("2RD");
+        sw.WriteLine("3ed");
+    }
+    using (StreamReader sr = new StreamReader(path, true))
+    {
+        string copy = sr.ReadLine();
+        while (copy != null)
+        {
+            using (StreamWriter sw = new StreamWriter(path2, true))
+            {
+                sw.WriteLine(copy);
+            }
+            copy = sr.ReadLine();
+        }
+    }
+    Console.WriteLine("Good job.");
+}
+catch(Exception e)
+{
+    Console.WriteLine(e);
+}
