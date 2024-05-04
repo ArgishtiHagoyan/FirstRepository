2faf2c57746f4dbf82cee0c227e23842e8d8de87
ArgishtiHagoyan <167971661+ArgishtiHagoyan@users.noreply.github.com>
2024-05-04 21:43:19 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,29 +1,64 @@
-﻿using System.IO;
-try
+using System;
+namespace sevagir
 {
-    string path = "/Users/artyom/Desktop/csharp/firstfile.txt";
-    string path2 = "/Users/artyom/Desktop/csharp/secondfile.txt";
-    string sms = "first line\nthe second line\nlast line";
+    public class Building
+    {
+        public int Floors;      // harkeri qanak
+        public int Area;        // taracq metrqarakusi
+        public int Occupants;   // bnakichneri qanak
+        public int area_for_onePerson;
+    }
 
-    using (StreamWriter sw = new StreamWriter(path))
+    public class BuildingDemo 
+
     {
-        sw.WriteLine(sms);
-    }
-    using (StreamReader sr = new StreamReader(path))
-    {
-        string copy = sr.ReadLine();
-        while (copy != null)
+       public static void Main()
+
         {
-            using (StreamWriter sw = new StreamWriter(path2, true))
+            try
             {
-                sw.WriteLine(copy);
+                Console.WriteLine("If you want input info abouth house then enter  word |house|," +
+                    " and if abouth hotel enter |hotel|");
+
+                string name = Convert.ToString(Console.ReadLine()!.ToLower());
+
+                if (name == "house")
+                {
+                    Building house = new Building();
+
+                    house.Occupants = Convert.ToInt32(Console.ReadLine());
+                    house.Area = Convert.ToInt32(Console.ReadLine());
+                    house.Floors = Convert.ToInt32(Console.ReadLine());
+                    house.area_for_onePerson = house.Area / house.Occupants;
+
+                    Console.WriteLine($"Shenqy uni {house.Area} qm." +
+                        $" Uni {house.Floors} hark." +
+                        $" Uni {house.Occupants} bnakaran" +
+                        $" Marda {house.area_for_onePerson} m2");
+
+                }
+                else if (name == "hotel")
+                {
+                    Building hotel = new Building();
+                    Console.WriteLine("Hark");
+                    hotel.Floors = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Taracq");
+                    hotel.Area = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Hyurer");
+                    hotel.Occupants = Convert.ToInt32(Console.ReadLine());
+                    hotel.area_for_onePerson = hotel.Area / hotel.Occupants;
+
+                    Console.WriteLine($"Uni {hotel.Floors} hark\n" +
+                        $"Taracqy {hotel.Area} qm\n" +
+                        $"Hyurer {hotel.Occupants}\n" +
+                        $"Mi hyur hamar nax. taracq {hotel.area_for_onePerson}qm");
+
+
+                }
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
             }
-            copy = sr.ReadLine();
-        }
+       }
     }
-    Console.WriteLine("Good job.");
-}
-catch(Exception e)
-{
-    Console.WriteLine(e);
 }
